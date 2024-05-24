150bb777d692fb708df616732a8c0e6de76007b8
Eddi3MS <edsonmarc2014@gmail.com>
2024-05-24 13:33:49 -0300

    text align

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <section className="flex flex-col items-center flex-1 justify-center">
-      <h1 className="text-blue-600 text-8xl font-bold">Timeline dos Santos</h1>
-      <h2 className="text-muted-foreground text-2xl">
+    <section className="flex flex-col items-center flex-1 justify-center px-4 text-center">
+      <h1 className="text-blue-600 text-5xl md:text-8xl font-bold">
+        Timeline dos Santos
+      </h1>
+      <h2 className="text-muted-foreground text-lg md:text-2xl">
         Os santos da Igreja Católica ordenados em uma linha temporal
       </h2>
     </section>
